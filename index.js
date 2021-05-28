5af9df6c4d1c742c4cababd5baa6477adf054edf
Aleksandr Ponomarenko <79024231+Ponomaleks@users.noreply.github.com>
2021-05-28 03:03:09 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,25 +1,161 @@
 console.log('Hello world!');
 
-// ================================== Задача 1 ======================================
+// // ================================== Задача 1 ======================================
+// const apartment = {
+//   imgUrl: 'https://via.placeholder.com/640x480',
+//   descr: 'Spacious apartment in the city center',
+//   rating: 4,
+//   price: 2153,
+//   tags: ['premium', 'promoted', 'top'],
+// };
+// // ================================== Задача 2 ======================================
+// const apartment = {
+//   imgUrl: 'https://via.placeholder.com/640x480',
+//   descr: 'Spacious apartment in the city center',
+//   rating: 4,
+//   price: 2153,
+//   tags: ['premium', 'promoted', 'top'],
+//   owner: {
+//     name: 'Henry',
+//     phone: '982-126-1588',
+//     email: 'henry.carter@aptmail.com',
+//   },
+// };
+// // ================================== Задача 3 ======================================
+// const apartment = {
+//   imgUrl: 'https://via.placeholder.com/640x480',
+//   descr: 'Spacious apartment in the city center',
+//   rating: 4,
+//   price: 2153,
+//   tags: ['premium', 'promoted', 'top'],
+// };
 
-// ================================== Задача 2 ======================================
+// // Change code below this line
+// const aptRating = apartment.rating;
+// const aptDescr = apartment.descr;
+// const aptPrice = apartment.price;
+// const aptTags = apartment.tags;
+// // Change code above this line
 
-// ================================== Задача 3 ======================================
+// // ================================== Задача 4 ======================================
+// const apartment = {
+//   imgUrl: 'https://via.placeholder.com/640x480',
+//   descr: 'Spacious apartment in the city center',
+//   rating: 4,
+//   price: 2153,
+//   tags: ['premium', 'promoted', 'top'],
+//   owner: {
+//     name: 'Henry',
+//     phone: '982-126-1588',
+//     email: 'henry.carter@aptmail.com',
+//   },
+// };
 
-// ================================== Задача 4 ======================================
+// // Change code below this line
+// const ownerName = apartment.owner.name;
+// const ownerPhone = apartment.owner.phone;
+// const ownerEmail = apartment.owner.email;
+// const numberOfTags = apartment.tags.length;
+// const firstTag = apartment.tags[0];
+// const lastTag = apartment.tags[apartment.tags.length - 1];
+// // Change code above this line
 
-// ================================== Задача 5 ======================================
+// // ================================== Задача 5 ======================================
+// const apartment = {
+//   imgUrl: 'https://via.placeholder.com/640x480',
+//   descr: 'Spacious apartment in the city center',
+//   rating: 4,
+//   price: 2153,
+//   tags: ['premium', 'promoted', 'top'],
+// };
+
+// // Change code below this line
+// const aptRating = apartment['rating'];
+// const aptDescr = apartment['descr'];
+// const aptPrice = apartment['price'];
+// const aptTags = apartment['tags'];
+// // Change code above this line
 
 // ================================== Задача 6 ======================================
+const apartment = {
+  imgUrl: 'https://via.placeholder.com/640x480',
+  descr: 'Spacious apartment in the city center',
+  rating: 4,
+  price: 2153,
+  tags: ['premium', 'promoted', 'top'],
+  owner: {
+    name: 'Henry',
+    phone: '982-126-1588',
+    email: 'henry.carter@aptmail.com',
+  },
+};
 
+// Change code below this line
+
+apartment.price = 5000;
+apartment.rating = 4.7;
+apartment.owner.name = 'Henry Sibola';
+apartment.tags.push('trusted');
+
+console.log(apartment);
 // ================================== Задача 7 ======================================
+const apartment = {
+  imgUrl: 'https://via.placeholder.com/640x480',
+  descr: 'Spacious apartment in the city center',
+  rating: 4.7,
+  price: 5000,
+  tags: ['premium', 'promoted', 'top', 'trusted'],
+  owner: {
+    name: 'Henry Sibola',
+    phone: '982-126-1588',
+    email: 'henry.carter@aptmail.com',
+  },
+};
 
+// Change code below this line
+apartment.area = 60;
+apartment.rooms = 3;
+apartment.location = { country: 'Jamaica', city: 'Kingston' };
 // ================================== Задача 8 ======================================
+const name = 'Repair Droid';
+const price = 2500;
+const image = 'https://via.placeholder.com/640x480';
+const tags = ['on sale', 'trending', 'best buy'];
 
+const product = {
+  // Change code below this line
+
+  name,
+  price,
+  image,
+  tags,
+
+  // Change code above this line
+};
 // ================================== Задача 9 ======================================
+const emailInputName = 'email';
+const passwordInputName = 'password';
 
+const credentials = {
+  // Change code below this line
+  [emailInputName]: 'henry.carter@aptmail.com',
+  [passwordInputName]: 'jqueryismyjam',
+
+  // Change code above this line
+};
 // ================================== Задача 10 ======================================
-
+const apartment = {
+  descr: 'Spacious apartment in the city center',
+  rating: 4,
+  price: 2153,
+};
+const keys = [];
+const values = [];
+// Change code below this line
+for (key in apartment) {
+  keys.push(key);
+  values.push(apartment[key]);
+}
 // ================================== Задача 11 ======================================
 
 // ================================== Задача 12 ======================================
