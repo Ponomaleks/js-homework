b24a9bc8ad5f228a21b46876ab5b6ac9e4f0c3e4
Aleksandr Ponomarenko <79024231+Ponomaleks@users.noreply.github.com>
2021-05-29 02:15:32 +0300

    Update index.js

--- index.js
+++ index.js
@@ -77,98 +77,185 @@ console.log('Hello world!');
 // // Change code above this line
 
 // ================================== Задача 6 ======================================
-const apartment = {
-  imgUrl: 'https://via.placeholder.com/640x480',
-  descr: 'Spacious apartment in the city center',
-  rating: 4,
-  price: 2153,
-  tags: ['premium', 'promoted', 'top'],
-  owner: {
-    name: 'Henry',
-    phone: '982-126-1588',
-    email: 'henry.carter@aptmail.com',
-  },
-};
+// const apartment = {
+//   imgUrl: 'https://via.placeholder.com/640x480',
+//   descr: 'Spacious apartment in the city center',
+//   rating: 4,
+//   price: 2153,
+//   tags: ['premium', 'promoted', 'top'],
+//   owner: {
+//     name: 'Henry',
+//     phone: '982-126-1588',
+//     email: 'henry.carter@aptmail.com',
+//   },
+// };
 
-// Change code below this line
+// // Change code below this line
 
-apartment.price = 5000;
-apartment.rating = 4.7;
-apartment.owner.name = 'Henry Sibola';
-apartment.tags.push('trusted');
+// apartment.price = 5000;
+// apartment.rating = 4.7;
+// apartment.owner.name = 'Henry Sibola';
+// apartment.tags.push('trusted');
 
-console.log(apartment);
-// ================================== Задача 7 ======================================
-const apartment = {
-  imgUrl: 'https://via.placeholder.com/640x480',
-  descr: 'Spacious apartment in the city center',
-  rating: 4.7,
-  price: 5000,
-  tags: ['premium', 'promoted', 'top', 'trusted'],
-  owner: {
-    name: 'Henry Sibola',
-    phone: '982-126-1588',
-    email: 'henry.carter@aptmail.com',
-  },
-};
+// console.log(apartment);
+// // ================================== Задача 7 ======================================
+// const apartment = {
+//   imgUrl: 'https://via.placeholder.com/640x480',
+//   descr: 'Spacious apartment in the city center',
+//   rating: 4.7,
+//   price: 5000,
+//   tags: ['premium', 'promoted', 'top', 'trusted'],
+//   owner: {
+//     name: 'Henry Sibola',
+//     phone: '982-126-1588',
+//     email: 'henry.carter@aptmail.com',
+//   },
+// };
 
-// Change code below this line
-apartment.area = 60;
-apartment.rooms = 3;
-apartment.location = { country: 'Jamaica', city: 'Kingston' };
-// ================================== Задача 8 ======================================
-const name = 'Repair Droid';
-const price = 2500;
-const image = 'https://via.placeholder.com/640x480';
-const tags = ['on sale', 'trending', 'best buy'];
+// // Change code below this line
+// apartment.area = 60;
+// apartment.rooms = 3;
+// apartment.location = { country: 'Jamaica', city: 'Kingston' };
+// // ================================== Задача 8 ======================================
+// const name = 'Repair Droid';
+// const price = 2500;
+// const image = 'https://via.placeholder.com/640x480';
+// const tags = ['on sale', 'trending', 'best buy'];
 
-const product = {
-  // Change code below this line
+// const product = {
+//   // Change code below this line
 
-  name,
-  price,
-  image,
-  tags,
+//   name,
+//   price,
+//   image,
+//   tags,
 
-  // Change code above this line
-};
-// ================================== Задача 9 ======================================
-const emailInputName = 'email';
-const passwordInputName = 'password';
+//   // Change code above this line
+// };
+// // ================================== Задача 9 ======================================
+// const emailInputName = 'email';
+// const passwordInputName = 'password';
 
-const credentials = {
-  // Change code below this line
-  [emailInputName]: 'henry.carter@aptmail.com',
-  [passwordInputName]: 'jqueryismyjam',
+// const credentials = {
+//   // Change code below this line
+//   [emailInputName]: 'henry.carter@aptmail.com',
+//   [passwordInputName]: 'jqueryismyjam',
 
-  // Change code above this line
-};
-// ================================== Задача 10 ======================================
-const apartment = {
-  descr: 'Spacious apartment in the city center',
-  rating: 4,
-  price: 2153,
-};
-const keys = [];
-const values = [];
-// Change code below this line
-for (key in apartment) {
-  keys.push(key);
-  values.push(apartment[key]);
-}
-// ================================== Задача 11 ======================================
+//   // Change code above this line
+// };
+// // ================================== Задача 10 ======================================
+// const apartment = {
+//   descr: 'Spacious apartment in the city center',
+//   rating: 4,
+//   price: 2153,
+// };
+// const keys = [];
+// const values = [];
+// // Change code below this line
+// for (key in apartment) {
+//   keys.push(key);
+//   values.push(apartment[key]);
+// }
+// // ================================== Задача 11 ======================================
+// const keys = [];
+// const values = [];
+// const advert = {
+//   service: 'apt',
+// };
+// const apartment = Object.create(advert);
+// apartment.descr = 'Spacious apartment in the city center';
+// apartment.rating = 4;
+// apartment.price = 2153;
 
-// ================================== Задача 12 ======================================
+// for (const key in apartment) {
+//   // Change code below this line
+//   if (apartment.hasOwnProperty(key)) {
+//     keys.push(key);
+//     values.push(apartment[key]);
+//   }
+//   // Change code above this line
+// }
 
-// ================================== Задача 13 ======================================
+// // ================================== Задача 12 ======================================
+// function countProps(object) {
+//   let propCount = 0;
+//   // Change code below this line
+//   for (prop in object) {
+//     if (object.hasOwnProperty(prop)) {
+//       propCount += 1;
+//     }
+//   }
+//   // Change code above this line
+//   return propCount;
+// }
 
-// ================================== Задача 14 ======================================
+// // ================================== Задача 13 ======================================
+// const apartment = {
+//   descr: 'Spacious apartment in the city center',
+//   rating: 4,
+//   price: 2153,
+// };
+// const values = [];
+// // Change code below this line
+// const keys = Object.keys(apartment);
+// for (key of keys) {
+//   values.push(apartment[key]);
+// }
 
-// ================================== Задача 15 ======================================
+// // ================================== Задача 14 ======================================
+// function countProps(object) {
+//   // Change code below this line
+//   let propCount = 0;
+
+//   const keys = Object.keys(object);
+
+//   for (key of keys) {
+//     propCount += 1;
+//   }
+
+//   return propCount;
+//   // Change code above this line
+// }
+
+// // ================================== Задача 15 ======================================
+// const apartment = {
+//   descr: 'Spacious apartment in the city center',
+//   rating: 4,
+//   price: 2153,
+// };
+// // Change code below this line
+// const keys = Object.keys(apartment);
+// const values = Object.values(apartment);
 
 // ================================== Задача 16 ======================================
+function countTotalSalary(salaries) {
+  let totalSalary = 0;
+  // Change code below this line
+  const salaryValues = Object.values(salaries);
+
+  for (value of salaryValues) {
+    totalSalary += value;
+  }
+  // Change code above this line
+  return totalSalary;
+}
 
 // ================================== Задача 17 ======================================
+const colors = [
+  { hex: '#f44336', rgb: '244,67,54' },
+  { hex: '#2196f3', rgb: '33,150,243' },
+  { hex: '#4caf50', rgb: '76,175,80' },
+  { hex: '#ffeb3b', rgb: '255,235,59' },
+];
+
+const hexColors = [];
+const rgbColors = [];
+// Change code below this line
+
+for (color of colors) {
+  hexColors.push(color.hex);
+  rgbColors.push(color.rgb);
+}
 
 // ================================== Задача 18 ======================================
 
