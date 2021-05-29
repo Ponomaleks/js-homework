009906a20490cc93aa29b16dc16469ed5c342b98
Aleksandr Ponomarenko <79024231+Ponomaleks@users.noreply.github.com>
2021-05-30 01:42:30 +0300

    Update index.js

--- index.js
+++ index.js
@@ -258,55 +258,279 @@ console.log('Hello world!');
 // }
 
 // ================================== Задача 18 ======================================
-const products = [
-  { name: 'Radar', price: 1300, quantity: 4 },
-  { name: 'Scanner', price: 2700, quantity: 3 },
-  { name: 'Droid', price: 400, quantity: 7 },
-  { name: 'Grip', price: 1200, quantity: 9 },
-];
+// const products = [
+//   { name: 'Radar', price: 1300, quantity: 4 },
+//   { name: 'Scanner', price: 2700, quantity: 3 },
+//   { name: 'Droid', price: 400, quantity: 7 },
+//   { name: 'Grip', price: 1200, quantity: 9 },
+// ];
 
-function getProductPrice(productName) {
-  // Change code below this line
-  let price = null;
-  for (product of products) {
-    if (product.name === productName) {
-      price = product.price;
-    }
-  }
-  return price;
+// function getProductPrice(productName) {
+//   // Change code below this line
+//   let price = null;
+//   for (product of products) {
+//     if (product.name === productName) {
+//       price = product.price;
+//     }
+//   }
+//   return price;
 
-  // Change code above this line
-}
+//   // Change code above this line
+// }
 
 // ================================== Задача 19 ======================================
+// const products = [
+//   { name: 'Radar', price: 1300, quantity: 4 },
+//   { name: 'Scanner', price: 2700, quantity: 3 },
+//   { name: 'Droid', price: 400, quantity: 7 },
+//   { name: 'Grip', price: 1200, quantity: 9 },
+// ];
 
-// ================================== Задача 20 ======================================
+// function getAllPropValues(propName) {
+//   // Change code below this line
 
-// ================================== Задача 21 ======================================
+//   const keys = [];
+//   for (product of products) {
+//     if (product[propName]) {
+//       keys.push(product[propName]);
+//     }
+//   }
+//   return keys;
+//   // Change code above this line
+// }
+// getAllPropValues('name');
+// // ================================== Задача 20 ======================================
+// const products = [
+//   { name: 'Radar', price: 1300, quantity: 4 },
+//   { name: 'Scanner', price: 2700, quantity: 3 },
+//   { name: 'Droid', price: 400, quantity: 7 },
+//   { name: 'Grip', price: 1200, quantity: 9 },
+// ];
 
-// ================================== Задача 22 ======================================
+// function calculateTotalPrice(productName) {
+//   // Пиши код ниже этой строки
 
-// ================================== Задача 23 ======================================
+//   let TotalPrice = 0;
+//   for (product of products) {
+//     if (product.name === productName) {
+//       TotalPrice = product.price * product.quantity;
+//     }
+//   }
 
-// ================================== Задача 24 ======================================
+//   return TotalPrice;
+//   // Пиши код выше этой строки
+// }
+
+// // ================================== Задача 21 ======================================
+// const highTemperatures = {
+//   yesterday: 28,
+//   today: 26,
+//   tomorrow: 33,
+// };
+// // Change code below this line
+// const { yesterday, today, tomorrow } = highTemperatures;
+
+// // Change code above this line
+// const meanTemperature = (yesterday + today + tomorrow) / 3;
+
+// // ================================== Задача 22 ======================================
+// const highTemperatures = {
+//   yesterday: 28,
+//   today: 26,
+//   tomorrow: 33,
+// };
+// // Change code below this line
+
+// const {
+//   yesterday,
+//   today,
+//   tomorrow,
+//   icon = 'https://www.flaticon.com/svg/static/icons/svg/2204/2204346.svg',
+// } = highTemperatures;
+
+// // Change code above this line
+// const meanTemperature = (yesterday + today + tomorrow) / 3;
+
+// // ================================== Задача 23 ======================================
+// const highTemperatures = {
+//   yesterday: 28,
+//   today: 26,
+//   tomorrow: 33,
+// };
+// // Change code below this line
+
+// const {
+//   yesterday: highYesterday,
+//   today: highToday,
+//   tomorrow: highTomorrow,
+//   icon: highIcon = 'https://www.flaticon.com/svg/static/icons/svg/2204/2204346.svg',
+// } = highTemperatures;
+
+// // Change code above this line
+// const meanTemperature = (highYesterday + highToday + highTomorrow) / 3;
+
+// // ================================== Задача 24 ======================================
+// const colors = [
+//   { hex: '#f44336', rgb: '244,67,54' },
+//   { hex: '#2196f3', rgb: '33,150,243' },
+//   { hex: '#4caf50', rgb: '76,175,80' },
+//   { hex: '#ffeb3b', rgb: '255,235,59' },
+// ];
+
+// const hexColors = [];
+// const rgbColors = [];
+// // Change code below this line
+
+// for (const { hex, rgb } of colors) {
+//   hexColors.push(hex);
+//   rgbColors.push(rgb);
+// }
 
 // ================================== Задача 25 ======================================
+// const forecast = {
+//   today: {
+//     low: 28,
+//     high: 32,
+//     icon: 'https://www.flaticon.com/svg/static/icons/svg/861/861059.svg',
+//   },
+//   tomorrow: {
+//     low: 27,
+//     high: 31,
+//   },
+// };
+// // Change code below this line
 
-// ================================== Задача 26 ======================================
+// const {
+//   today: {
+//     low: lowToday,
+//     high: highToday,
+//     icon: todayIcon = 'https://www.flaticon.com/svg/static/icons/svg/2204/2204346.svg',
+//   },
+//   tomorrow: {
+//     low: lowTomorrow,
+//     high: highTomorrow,
+//     icon: tomorrowIcon = 'https://www.flaticon.com/svg/static/icons/svg/2204/2204346.svg',
+//   },
+// } = forecast;
+// // ================================== Задача 26 ======================================
+// // Change code below this line
+// function calculateMeanTemperature(forecast) {
+//   const {
+//     today: { low: todayLow, high: todayHigh },
+//     tomorrow: { low: tomorrowLow, high: tomorrowHigh },
+//   } = forecast;
 
-// ================================== Задача 27 ======================================
+//   // Change code above this line
+//   return (todayLow + todayHigh + tomorrowLow + tomorrowHigh) / 4;
+// }
 
-// ================================== Задача 28 ======================================
+// // ================================== Задача 27 ======================================
+// const scores = [89, 64, 42, 17, 93, 51, 26];
+// // Change code below this line
+// const bestScore = Math.max(...scores);
+// const worstScore = Math.min(...scores);
 
-// ================================== Задача 30 ======================================
+// // ================================== Задача 28 ======================================
+// const firstGroupScores = [64, 42, 93];
+// const secondGroupScores = [89, 14, 51, 26];
+// const thirdGroupScores = [29, 47, 18, 97, 81];
+// // Change code below this line
+// const allScores = [
+//   ...firstGroupScores,
+//   ...secondGroupScores,
+//   ...thirdGroupScores,
+// ];
+// const bestScore = Math.max(...allScores);
+// const worstScore = Math.min(...allScores);
 
-// ================================== Задача 31 ======================================
+// // ================================== Задача 29 ======================================
+
+// const defaultSettings = {
+//   theme: 'light',
+//   public: true,
+//   withPassword: false,
+//   minNumberOfQuestions: 10,
+//   timePerQuestion: 60,
+// };
+// const overrideSettings = {
+//   public: false,
+//   withPassword: true,
+//   timePerQuestion: 30,
+// };
+// // Change code below this line
+// const finalSettings = { ...defaultSettings, ...overrideSettings };
+
+// // ================================== Задача 30 ======================================
+
+// function makeTask(data) {
+//   const completed = false;
+//   const category = 'General';
+//   const priority = 'Normal';
+//   // Change code below this line
+//   return { completed, category, priority, ...data };
+//   // Change code above this line
+// }
+// // ================================== Задача 31 ======================================
+
+// Change code below this line
+// function add(...args) {
+//   let sum = 0;
+
+//   for (arg of args) {
+//     sum = sum += arg;
+//   }
+//   // Change code above this line
+// }
+// add(32, 6);
 
 // ================================== Задача 32 ======================================
+// // Change code below this line
+// function addOverNum(firtArg, ...args) {
+//   let total = 0;
 
-// ================================== Задача 33 ======================================
+//   for (const arg of args) {
+//     if (arg > firtArg) {
+//       total += arg;
+//     }
+//   }
 
+//   return total;
+//   // Change code above this line
+// }
+// // ================================== Задача 33 ======================================
+
+// Change code below this line
+function findMatches(array, ...rest) {
+  const matches = []; // Don't change this line
+
+  for (arg of rest) {
+    if (array.includes(arg)) {
+      matches.push(arg);
+    }
+  }
+  // Change code above this line
+  return matches;
+}
 // ================================== Задача 34 ======================================
+
+const bookShelf = {
+  // Change code below this line
+  books: ['The last kingdom', 'The guardian of dreams'],
+  getBooks() {
+    return 'Returning all books';
+  },
+  addBook(bookName) {
+    return `Adding book ${bookName}`;
+  },
+  removeBook(bookName) {
+    return `Deleting book ${bookName}`;
+  },
+  updateBook(oldName, newName) {
+    return `Updating book ${oldName} to ${newName}`;
+  },
+
+  // Change code above this line
+};
 
 // ================================== Задача 35 ======================================
 
