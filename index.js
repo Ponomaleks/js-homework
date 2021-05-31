5decb9c6578962d9fa2b7370ea2a0ef34e927e17
Aleksandr Ponomarenko <79024231+Ponomaleks@users.noreply.github.com>
2021-05-31 23:07:09 +0300

    Update index.js

--- index.js
+++ index.js
@@ -123,19 +123,14 @@ const orders = [
   { email: 'artemis@coldmail.net', dish: 'Pizza' },
   { email: 'jacob@mail.com', dish: 'Taco' },
 ];
-
 // Пиши код ниже этой строки
 function composeMessage(position) {
-  return `Готовим ${this[position - 1].dish} для ${
-    this[position - 1].email
-  }. Ваш заказ ${position}-й в очереди.`;
+  return `Готовим ${this.dish} для ${this.email}. Ваш заказ ${position}-й в очереди.`;
 }
 const messages = [];
-
 for (let i = 0; i < orders.length; i += 1) {
-  messages.push(composeMessage.call(orders, i + 1));
+  messages.push(composeMessage.call(orders[i], i + 1));
 }
-console.log(messages);
 // ================================== Задача 8 ======================================
 const orders = [
   { email: 'solomon@topmail.ua', dish: 'Burger' },
