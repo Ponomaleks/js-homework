be58fc844dd0eaf2aadb32732f23d8d7b43f8304
Aleksandr Ponomarenko <79024231+Ponomaleks@users.noreply.github.com>
2021-06-07 22:42:07 +0300

    1-5 tasks done

--- index.js
+++ index.js
@@ -1,15 +1,66 @@
 console.log('Hello world!');
 
 // ================================== Задача 1 ======================================
+const parent = {
+  name: 'Stacey',
+  surname: 'Moore',
+  age: 54,
+  heritage: 'Irish',
+};
+// Пиши код ниже этой строки
 
+const child = Object.create(parent);
+
+// Пиши код выше этой строки
+child.name = 'Jason';
+child.age = 27;
 // ================================== Задача 2 ======================================
+const ancestor = {
+  name: 'Paul',
+  age: 83,
+  surname: 'Dawson',
+  heritage: 'Irish',
+};
+// Пиши код ниже этой строки
 
+const parent = Object.create(ancestor);
+parent.name = 'Stacey';
+parent.surname = 'Moore';
+parent.age = 54;
+
+const child = Object.create(parent);
+child.name = 'Jason';
+child.age = 27;
+
+// Пиши код выше этой строки
 // ================================== Задача 3 ======================================
+function Car(brand, model, price) {
+  this.brand = brand;
+  this.model = model;
+  this.price = price;
+}
 
 // ================================== Задача 4 ======================================
+function Car({ brand, model, price }) {
+  this.brand = brand;
+  this.model = model;
+  this.price = price;
+}
 
 // ================================== Задача 5 ======================================
+function Car({ brand, model, price }) {
+  this.brand = brand;
+  this.model = model;
+  this.price = price;
+}
 
+Car.prototype.getPrice = function () {
+  return this.price;
+};
+
+Car.prototype.changePrice = function (newPrice) {
+  this.price = newPrice;
+};
 // ================================== Задача 6 ======================================
 
 // ================================== Задача 7 ======================================
